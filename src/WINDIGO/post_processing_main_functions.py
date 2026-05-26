3390ead3c32e8cdefb49452fd137f2a56f05241a
jacobwisienski86 <jacobwisienski86@gmail.com>
2026-05-25 19:11:52 -0600

    Compartmentalized uncertainty calculations

--- src/WINDIGO/post_processing_main_functions.py
+++ src/WINDIGO/post_processing_main_functions.py
@@ -126,7 +126,7 @@ def generate_relative_sensitivity_plot(
                                                                    original_inputs=original_inputs,
                                                                    relative_flag=relative_flag)
 
-   else:
+   elif sens_calculation_method == 'Central':
       relative_sens_coefficients = calculate_central_coefficients(positive_perturbed_outputs=positive_perturbed_outputs,
                                                                   negative_perturbed_outputs=negative_perturbed_outputs,
                                                                   unperturbed_output=unperturbed_output,
@@ -135,13 +135,18 @@ def generate_relative_sensitivity_plot(
                                                                   original_inputs=original_inputs,
                                                                   perturbation_coefficient=perturbation_coefficient,
                                                                   relative_flag=relative_flag)
+   else:
+      raise Exception("Invalid sensitivity coefficient calculation method. Please use either 'Forward', 'Backward', or 'Central'. ")
+
+   #Calculate the relative sensitivity per unit lethargy
 
    relative_sens_per_lethargy = convert_per_lethargy(relative_sens_coefficients=relative_sens_coefficients,
                                                      energy_grid_MeV=energy_grid_MeV)
    
+   #Generate the relative sensitivity plot
+
    plot_relative_sens(relative_sens_per_lethargy=relative_sens_per_lethargy,
                       energy_grid_MeV=energy_grid_MeV)
-
 
 
 def calculate_direct_perturbation_uncertainty(
@@ -155,15 +160,24 @@ def calculate_direct_perturbation_uncertainty(
       negative_perturbed_inputs=[],
       perturbation_coefficient=1.0,
 ):
-   
+   #Check that the inputted covariance matrix is square
+
    if (np.shape(covariance_matrix)[0] != np.shape(covariance_matrix)[1]):
       raise Exception('Invalid covariance matrix shape. A square matrix is required.')
    
+   #Check that the length of the sensitivity array is the same as the number of rows/columns in the covariance matrix
+
    if np.shape(covariance_matrix)[0] != len(original_inputs):
       raise Exception('Invalid data shapes. The number of rows/columns in'
       'the covariance matrix must be the same as the length of the unperturbed'
       'inputs array.')
    
+   #Set the flag to calculate absolute sensitivity coefficients 
+
+   relative_flag = False
+   
+   #Check if data inputs have the right shapes based on the sensitivity coefficient calculation method, and calculate the sensitivity coefficients
+
    if sens_calculation_method == 'Forward':
 
       if (len(positive_perturbed_outputs) != len(original_inputs) or
@@ -171,7 +185,11 @@ def calculate_direct_perturbation_uncertainty(
          raise Exception('Invalid data shapes. All inputs and outputs arrays' \
          'must have the same length')
       
-      absolute_sensitivity_coefficients = (positive_perturbed_outputs - unperturbed_output)/(positive_perturbed_inputs - original_inputs)
+      absolute_sensitivity_coefficients = calculate_forward_coefficients(positive_perturbed_outputs = positive_perturbed_outputs,
+                                                                  unperturbed_output = unperturbed_output,
+                                                                  positive_perturbed_inputs = positive_perturbed_inputs,
+                                                                  original_inputs = original_inputs,
+                                                                  relative_flag = relative_flag)
 
    elif sens_calculation_method == 'Backward':
 
@@ -180,7 +198,11 @@ def calculate_direct_perturbation_uncertainty(
          raise Exception('Invalid data shapes. All inputs and outputs arrays' \
          'must have the same length')
       
-      absolute_sensitivity_coefficients = (unperturbed_output - negative_perturbed_outputs)/(original_inputs - negative_perturbed_inputs)
+      absolute_sensitivity_coefficients = calculate_backward_coefficients(negative_perturbed_outputs=negative_perturbed_outputs,\
+                                                                   unperturbed_output=unperturbed_output,
+                                                                   negative_perturbed_inputs=negative_perturbed_inputs,
+                                                                   original_inputs=original_inputs,
+                                                                   relative_flag=relative_flag)
       
    elif sens_calculation_method == 'Central':
 
@@ -191,12 +213,23 @@ def calculate_direct_perturbation_uncertainty(
          raise Exception('Invalid data shapes. All inputs and outputs arrays' \
          'must have the same length')
       
-      absolute_sensitivity_coefficients = (positive_perturbed_outputs - positive_perturbed_inputs)/(2*perturbation_coefficient*original_inputs)
+      absolute_sensitivity_coefficients = calculate_central_coefficients(positive_perturbed_outputs=positive_perturbed_outputs,
+                                                                  negative_perturbed_outputs=negative_perturbed_outputs,
+                                                                  unperturbed_output=unperturbed_output,
+                                                                  positive_perturbed_inputs=positive_perturbed_inputs,
+                                                                  negative_perturbed_inputs=negative_perturbed_inputs,
+                                                                  original_inputs=original_inputs,
+                                                                  perturbation_coefficient=perturbation_coefficient,
+                                                                  relative_flag=relative_flag)
       
    else:
       raise Exception('Invalid sensitivity coefficient calculation method')
    
+   #Calculate the uncertainty using the sensitivity coefficients and covariance matrix
+   
    propagated_uncertainty = np.sqrt(absolute_sensitivity_coefficients @ covariance_matrix @ absolute_sensitivity_coefficients.T)
+
+   #Print the calculate unceratainty
 
    print('The direct perturbatation uncertainty is: ' + str(propagated_uncertainty))
 
@@ -206,46 +239,23 @@ def calculate_random_sampling_uncertainty(
       perturbed_outputs
 ):
    
+   #Check that the type of perturbed_outputs is np.ndarray, and change to it if necessary
    if type(perturbed_outputs) is not np.ndarray:
       perturbed_outputs = np.array(perturbed_outputs)
    
+   #Find the total number of perturbed outputs
    n = len(perturbed_outputs)
 
+   #Calculate the mean of the perturbed outputs
    mean_output = np.mean(perturbed_outputs)
 
+   #Find the squared distances between the individual perturbed outputs and the mean perturbed output
    squared_distances = (perturbed_outputs - mean_output)**2
 
+   #Calculate the random sampling uncertainty
    propagated_uncertainty = np.sqrt(np.sum(squared_distances)/(n-1))
 
+   #Print the random sampling uncertainty 
    print('The random sampling uncertainty is: ' + str(propagated_uncertainty))
 
    return propagated_uncertainty
-
-scale44_grid_MeV = np.array([
-    1.0000E-11, 3.0000E-9, 7.5000E-9, 1.0000E-8, 2.5300E-8,
-    3.0000E-8, 4.0000E-8, 5.0000E-8, 7.0000E-8, 1.0000E-7,
-    1.5000E-7, 2.0000E-7, 2.2500E-7, 2.5000E-7, 2.7500E-7,
-    3.2500E-7, 3.5000E-7, 3.7500E-7, 4.0000E-7, 6.2500E-7,
-    1.0000E-6, 1.7700E-6, 3.0000E-6, 4.7500E-6, 6.0000E-6,
-    8.1000E-6, 1.0000E-5, 3.0000E-5, 1.0000E-4, 5.5000E-4,
-    3.0000E-3, 1.7000E-2, 2.5000E-2, 1.0000E-1, 4.0000E-1,
-    9.0000E-1, 1.4000E0, 1.8500E0, 2.3540E0, 2.4790E0,
-    3.0000E0, 4.8000E0, 6.4340E0, 8.1873E0, 2.0000E1
-])
-
-
-generate_relative_sensitivity_plot(energy_grid_MeV=scale44_grid_MeV,
-                                   unperturbed_output=np.load('examples/Jezebel_BaseKeff_Value.npy'),
-                                   sens_calculation_method='Forward',
-                                   original_inputs=np.load('examples/Original_Pu239Fission_xs.npy'),
-                                   positive_perturbed_outputs=np.load('examples/Jezebel_DirectKeff_Values.npy'),
-                                   positive_perturbed_inputs=np.load('examples/Perturbed_Pu239Fission_xs.npy'))
-
-calculate_direct_perturbation_uncertainty(sens_calculation_method='Forward',
-                                   unperturbed_output=np.load('examples/Jezebel_BaseKeff_Value.npy'),
-                                   original_inputs=np.load('examples/Original_Pu239Fission_xs.npy'),
-                                   positive_perturbed_outputs=np.load('examples/Jezebel_DirectKeff_Values.npy'),
-                                   positive_perturbed_inputs=np.load('examples/Perturbed_Pu239Fission_xs.npy'),
-                                   covariance_matrix=np.loadtxt(open('examples/covarianceMatrix_45Groups_Pu239_MT_18_Absolute.csv', "rb"), delimiter=","))            
-
-calculate_random_sampling_uncertainty(perturbed_outputs=np.load('examples/Jezebel_RandomKeff_Values.npy'))
